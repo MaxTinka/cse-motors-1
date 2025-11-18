29258c1adf591600685b04f4748ca2742c0e968a
MaxTinka <tinkamax03@gmail.com.com>
2025-11-18 21:18:36 +0300

    wk2

--- server.js
+++ server.js
@@ -6,32 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const expressLayouts = require("express-ejs-layouts")
-
 
 /* ***********************
- * View Engine and Templates
+ * View engine templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
-
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 
-/* ***********************
- * Index route
- *************************/
-app.get("/", function(req, res) {
-  res.render("index", { title: "Welcome" })
+//Index route
+app.get("/" , function(req, res){
+  res.render("index", {title: "Home"})
 })
-
 
 /* ***********************
  * Local Server Information
